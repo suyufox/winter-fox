84ce83d19c1fc4655abb48b20c020815fb0b17a0
suyufox <shenming26@outlook.com>
2025-05-07 11:14:20 +0800

    create

---
+++ winter-fox/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace winter_fox
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
